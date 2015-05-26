36132cf7cae844a005192c914b66c21069b612fa
Chakresh Jain <jain.chakresh@gmail.com>
2015-05-26 19:57:07 +1200

    diskdb instead of mongodb
    Updated the code to use diskdb instead of mongodb

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,51 +1,39 @@
 (function() {
-
+ 
   'use strict';
   var express = require('express');
   var router = express.Router();
-  var mongojs = require('mongojs');
-  var db = mongojs('mongodb://admin:admin123@ds063809.mongolab.com:63809/meantodo', ['todos']);
-
+  var db = require('diskdb');
+  db = db.connect('server/db', ['todos']);
+ 
   /* GET home page. */
   router.get('/', function(req, res) {
     res.render('index');
   });
-
+ 
   router.get('/api/todos', function(req, res) {
-    db.todos.find(function(err, data) {
-      res.json(data);
-    });
+    res.json(db.todos.find());
   });
-
+ 
   router.post('/api/todos', function(req, res) {
-    db.todos.insert(req.body, function(err, data) {
-      res.json(data);
-    });
-
+    res.json(db.todos.save(req.body));
   });
-
+ 
   router.put('/api/todos', function(req, res) {
-
-    db.todos.update({
-      _id: mongojs.ObjectId(req.body._id)
+    res.json(db.todos.update({
+      _id: req.body._id
     }, {
       isCompleted: req.body.isCompleted,
       todo: req.body.todo
-    }, {}, function(err, data) {
-      res.json(data);
-    });
-
+    }));
   });
-
+ 
   router.delete('/api/todos/:_id', function(req, res) {
-    db.todos.remove({
-      _id: mongojs.ObjectId(req.params._id)
-    }, '', function(err, data) {
-      res.json(data);
-    });
-
+    res.json(db.todos.remove({
+      _id: req.params._id
+    }));
   });
-
+ 
   module.exports = router;
-
+ 
 }());
